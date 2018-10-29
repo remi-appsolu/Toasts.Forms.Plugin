027125173f4a1ee7b3b6ccdde63d1254f91783f1
Remi Brun <remi.brun@appsolu.net>
2018-10-29 23:15:41 +0100

    Added build script

---
+++ build.bat
@@ -0,0 +1,5 @@
+REM msbuild Toasts.Forms.Plugin.sln /t:Clean;Build /p:Configuration=Release
+
+del *.nupkg
+nuget pack Misc\Toasts.Forms.Plugin.nuspec
+nuget.exe push -Source "AppsoluTaxi" -ApiKey VSTS *.nupkg
